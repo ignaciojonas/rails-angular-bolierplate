f6af97cf8c8dc34e1480191971da01e320a9b5fb
Ignacio Jonas <ignacio.jonas@eclinicalos.com>
2015-06-15 23:17:11 -0300

    All the requests to the same template, leave angular-ui-routes to route the requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
    mount Api::Endpoints => '/api'
-
+   constraints ->(request) { request.format == 'html' } do
+       match "*path" => 'home#index', via: :get
+     end
    root 'home#index'
 end
